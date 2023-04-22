fee9421313ff4ff8e0401388b493cd41536f4366
Lucas-Akiama <lucas.brasiliano@sptech.school>
2023-04-22 19:32:15 -0300

    cadastro funcionario funcionando

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sql = require('mssql');
 var mySqlConfig = {
     host: "localhost",
     database: "monitority",
-    user: "root",
-    password: "urubu100",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
-
+var funcionarioController = require("../controllers/funcionarioController")
 router.get("/", function (req, res) {
     usuarioController.testar(req, res);
 });
@@ -19,5 +19,7 @@ router.post("/cadastrar", function (req, res) {
 router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
-
+router.post("/cadastrarfuncionario", function (req, res) {
+    funcionarioController.cadastrarFuncionario(req, res);
+})
 module.exports = router;
